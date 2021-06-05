c7b1c7d861e2c4d5dbf7333687f44de316aef502
JpabloMF <juanpablo45671@gmail.com>
2021-06-05 17:56:50 -0500

    Adding form and validation to FPassword component

--- src/views/login/forgotPassword/index.jsx
+++ src/views/login/forgotPassword/index.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import Button from '../../../components/button/index';
 import Input from '../../../components/input/index';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useHistory } from 'react-router-dom';
+
+const ForgotPasswordSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Email is required'),
+});
 
 const ForgotPassword = () => {
+  const history = useHistory();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      history.push('/');
+    },
+    validationSchema: ForgotPasswordSchema,
+    validateOnChange: false,
+  });
+
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={formik.handleSubmit}>
       <Input
         name="email"
         title="Enter your email address"
         type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
         size="big"
         width="100%"
         autoComplete="on"
+        error={formik?.errors?.email}
       />
       <Button size="large" color="orange" label="Send password reset email" />
     </form>
