13a1627a509242ff8d51c64e1bc838e546530296
JpabloMF <juanpablo45671@gmail.com>
2021-06-05 11:49:35 -0500

    Initial theme implementation * Implementing ThemeProvider in the App.js root file * Creating theme.js file

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -8,19 +9,23 @@ import Login from '../views/login/index';
 import Dashboard from '../views/dashboard';
 import PageNotFound from '../views/pageNotFound';
 
+import theme from '../utils/theme';
+
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login/" component={Login} />
-            <Route path="/dashboard/" component={Dashboard} />
-            <Route component={PageNotFound} />
-          </Switch>
-        </Router>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/login/" component={Login} />
+              <Route path="/dashboard/" component={Dashboard} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </Router>
+        </Provider>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  white: '#ffffff',
+  black: '#000000',
+  yellow: '#fcc738',
+  red: '#ff5245',
+  blue: '#29808e',
+  background: '#212121',
+  grey: '#25272b',
+  darkGrey: '#232325',
+};
+
+export default theme;
